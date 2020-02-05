0e23345bc9e070603fb21ffe3048ff3d4bd272b0
Sudirman <dirman.elektro8@gmail.com>
2020-02-06 05:45:59 +0700

    menambahkan file robofriend dari direktori baru (direktori G)

---
+++ build/precache-manifest.88b60b09cbc45572272697a33f527532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa51fcaefb2fbda7e11fc05ec52d2e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b198c3472fc2a472f0",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "461c3753dbe3f7cd035f",
+    "url": "/static/css/main.ea6e7134.chunk.css"
+  },
+  {
+    "revision": "b5b198c3472fc2a472f0",
+    "url": "/static/js/2.97c170a6.chunk.js"
+  },
+  {
+    "revision": "461c3753dbe3f7cd035f",
+    "url": "/static/js/main.a13176a8.chunk.js"
+  },
+  {
+    "revision": "4f817df7fe92cf502ade",
+    "url": "/static/js/runtime-main.2d96d986.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
